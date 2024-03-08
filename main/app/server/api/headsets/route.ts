bcdcb7fc0f8c5032275c4f7047ed6c86e271b77e
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2024-03-07 20:39:20 -0800

    Basic get request for headset

---
+++ main/app/server/api/headsets/route.ts
@@ -0,0 +1,31 @@
+import { db } from "@/app/server/db/index";
+import { NextResponse } from "next/server";
+import { Headset, headsets } from "@/app/server/db/schema/headsets";
+
+/**
+ * @returns {Promise<Headset[]>}
+ * @description Fetches all headsets from the database
+ */
+async function fetchHeadsets(): Promise<Headset[]>{
+  const query = db
+    .select()
+    .from(headsets)
+  const result = await query.execute();
+  return result;
+}
+
+/**
+ * @param {Request} request
+ * @param {Response} response
+ * @returns {Promise<Response>}
+ * @description Fetches all headsets from the database and returns a response
+ */
+export async function GET(request: Request, response: Response) {
+  try {
+    const headsetsQuery = await fetchHeadsets();  
+    return NextResponse.json(headsetsQuery);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
+}
--- main/app/server/db/index.ts
+++ main/app/server/db/index.ts
@@ -1,8 +1,5 @@
 import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
-import { users } from './schema/users';
 
 const sqlite = new Database('main.db');
 export const db: BetterSQLite3Database = drizzle(sqlite);
-
-const allUsers = db.select().from(users).all();
