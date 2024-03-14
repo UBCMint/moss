c3121b3c68381623d44ec5fe5e14f3dd5b528b9c
Jayden Piao <71306047+jaydenpiao@users.noreply.github.com>
2024-03-14 14:43:57 -0700

    jaydenpiao/neu-19-get-all-users (#3)
    * created get all users route
    
    * update
    
    * add return type

---
+++ main/app/server/api/users/getAllUsers/route.ts
@@ -0,0 +1,31 @@
+import { db } from "@/app/server/db/index";
+import { NextResponse } from "next/server";
+import { User, users } from "@/app/server/db/schema/users";
+
+/**
+ * @returns {Promise<User[]>}
+ * @description Fetches all users from the database
+ */
+async function fetchUsers(): Promise<User[]>{
+    const query = db
+      .select()
+      .from(users)
+    const result = await query.execute();
+    return result;
+  }
+
+/**
+ * @param {Request} request
+ * @param {Response} response
+ * @returns {Promise<Response>}
+ * @description Fetches all users from the database and returns a response
+ */
+export async function GET(request: Request, response: Response): Promise<Response> {
+    try {
+      const usersQuery = await fetchUsers();  
+      return NextResponse.json(usersQuery);
+    } catch (error) {
+      console.error(error);
+      return NextResponse.error();
+    }
+  }
