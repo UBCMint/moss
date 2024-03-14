2862000830dae9534051cb4d94cbd76a6243d790
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2024-03-14 10:53:49 -0700

    Headset Get Route Cleanup

--- main/app/server/api/headsets/route.ts
+++ main/app/server/api/headsets/route.ts
@@ -1,17 +1,17 @@
-import { db } from "@/app/server/db/index";
-import { NextResponse } from "next/server";
-import { Headset, headsets } from "@/app/server/db/schema/headsets";
+import { db } from '@/app/server/db/index'
+import { NextResponse } from 'next/server'
+import { type Headset, headsets } from '@/app/server/db/schema/headsets'
 
 /**
  * @returns {Promise<Headset[]>}
  * @description Fetches all headsets from the database
  */
-async function fetchHeadsets(): Promise<Headset[]>{
+async function fetchHeadsets (): Promise<Headset[]> {
   const query = db
     .select()
     .from(headsets)
-  const result = await query.execute();
-  return result;
+  const result = await query.execute()
+  return result
 }
 
 /**
@@ -20,12 +20,11 @@ async function fetchHeadsets(): Promise<Headset[]>{
  * @returns {Promise<Response>}
  * @description Fetches all headsets from the database and returns a response
  */
-export async function GET(request: Request, response: Response) {
+export async function GET (request: Request, response: Response): Promise<Response> {
   try {
-    const headsetsQuery = await fetchHeadsets();  
-    return NextResponse.json(headsetsQuery);
+    const headsetsQuery = await fetchHeadsets()
+    return NextResponse.json(headsetsQuery)
   } catch (error) {
-    console.error(error);
-    return NextResponse.error();
+    return NextResponse.error()
   }
 }
