10680cebe135b3a894616192c98d6acc748a9576
mona187 <monaabusharkh18@gmail.com>
2024-03-17 11:06:42 -0700

    slight change

--- main/app/server/api/user/deleteuser/[id]/route.ts
+++ main/app/server/api/user/deleteuser/[id]/route.ts
@@ -25,19 +25,19 @@ export async function DELETE (request: Request) {
   try {
 
     const id = request.url.split('/').pop() as string;
-
-    if (isNaN(parseInt(id))) {
+    const number_id = parseInt(id)
+    if (isNaN(number_id)) {
       return NextResponse.json({ error: 'Invalid ID provided' }, { status: 400 });
     }
 
     const deletedUsers: User[] = 
-      await db.delete(users).where(eq(users.id, parseInt(id))).returning();
+      await db.delete(users).where(eq(users.id, number_id)).returning();
       
       
-    return NextResponse.json({ deletedUsers });
+    return NextResponse.json({ deletedUsers }, { status: 200 });
 
   } catch (error) {
-    return NextResponse.json(error);
+    return NextResponse.json({ error }, { status: 500});
   }
 }
 
