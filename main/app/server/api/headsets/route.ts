6963dcaadf25cf6671438b0f33d10d202b6d6283
jaydenpiao <jaydenpiao@gmail.com>
2024-03-23 00:02:08 -0700

    addHeadset function

--- main/app/server/api/headsets/route.ts
+++ main/app/server/api/headsets/route.ts
@@ -28,3 +28,16 @@ export async function GET (request: Request, response: Response): Promise<Respon
     return NextResponse.error()
   }
 }
+
+// POST request should add a new row to the database. Ensure the data being added to the 
+// database is of valid type for the specific columns its being added to.
+
+/**
+ * @returns null
+ * @description Adds a new headset to the database
+ */
+async function addHeadset (id: number, name: string, description: string, channelNumber: number, channelList: string, purpose: string, portability: string, price: number, company: string, batteryLife: string) {
+  await db.insert(headsets).values({id: id, name: name, description: description, channelNumber: channelNumber, channelList: channelList, purpose: purpose, portability: portability, price: price, company: company, batteryLife: batteryLife});
+}
+
+
