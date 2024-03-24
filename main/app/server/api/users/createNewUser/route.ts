1a7dac893351b62ec9f7f28a38cff95aef8218ea
Michael Cui <122322432+mchlcui@users.noreply.github.com>
2024-03-24 07:40:56 -0700

    updated requested PR change

--- main/app/server/api/users/createNewUser/route.ts
+++ main/app/server/api/users/createNewUser/route.ts
@@ -2,6 +2,10 @@ import { db } from "@/app/server/db";
 import { users } from "@/app/server/db/schema/users";
 import {NextApiRequest, NextApiResponse} from "next";
 
+/**
+ * @param {String}
+ * @description List of possible roles
+ */
 
 let roles = new Set<string>(["Software Engineer", "Product Manager", "Electrical Engineer"])
 
@@ -20,16 +24,16 @@ async function addUser(username: string, password: string, email: string, role: 
  * @description Adds a new user to the database and returns a response
  */
 export async function POST(request: NextApiRequest, response: NextApiResponse) {
-    const input = await request.json();
-    const username = input.username;
-    const password = input.password;
-    const email = input.email;
-    const role = input.role;
+    const data = await request.json();
+    const username = data.username;
+    const password = data.password;
+    const email = data.email;
+    const role = data.role;
 
     try {
         if (typeof username != 'string' || typeof password != 'string' ||
             typeof email != 'string' || typeof role != 'string' || !roles.has(role)) {
-            return new Response( JSON.stringify("missing parameters or invalid role") ,{ status:400 } )
+            return new NextResponse("invalid type of parameters");
         }
         await addUser(username as string, password as string, email as string, role as string);
         return new Response( JSON.stringify("user created") ,{ status:200 } )
