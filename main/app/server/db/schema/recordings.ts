80b682282a6d73b5c42f0a7ae7bf792f09e287fa
Tammy Kim <tammykim00723@gmail.com>
2024-03-30 22:41:33 -0700

    Fixed the field name from timestamp to time

--- main/app/server/db/schema/recordings.ts
+++ main/app/server/db/schema/recordings.ts
@@ -4,7 +4,7 @@ import { headsets } from './headsets'
 export const recordings = sqliteTable('recordings', {
   headset: integer('headset').references(() => headsets.id),
   name: text('name', { length: 256 }),
-  timestamp: text('timestamp')
+  time: text('time')
 })
 
 export type Recording = typeof recordings.$inferSelect
