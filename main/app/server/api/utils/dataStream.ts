e01e545f1dc4f5d9aefa241942829bb436ad81fc
jaydenpiao <jaydenpiao@gmail.com>
2024-03-30 23:08:59 -0700

    function to send 100 integers/second

--- main/app/server/api/utils/dataStream.ts
+++ main/app/server/api/utils/dataStream.ts
@@ -3,7 +3,7 @@ import { ServerEventMessage, ServerEventsClient } from "@servicestack/client"
 /**
  * @param {ServerEventMessage} msg
  * @returns {void}
- * @description Logs the integer received from the server
+ * @description Connects to event stream and logs the integer received from the server
  */
 const client = new ServerEventsClient("/", ["*"], {
     onMessage: (msg:ServerEventMessage) => {
@@ -16,3 +16,18 @@ const client = new ServerEventsClient("/", ["*"], {
 }).start()
 
 export default client
+
+/**
+ * @param {number} integer
+ * @returns {void}
+ * @description Sends integers to client 
+ */
+export const subscribeToIntegers = (callback: (integer: number) => void) => {
+    let counter = 0
+    const intervalId = setInterval(() => {
+        callback(counter++)
+        if (counter > 99) counter = 0
+    }, 10) // send integer every 10ms => 100 integers per second
+    // clean up
+    return () => clearInterval(intervalId)
+}
