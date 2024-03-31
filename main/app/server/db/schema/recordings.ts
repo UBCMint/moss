b54df20eb755bb5449cbe741780bf6f90b34b0ac
Tammy Kim <tammykim00723@gmail.com>
2024-03-30 22:38:51 -0700

    Create a get request API that lets you get all the data information on headset recordings from the database

---
+++ main/app/server/db/schema/recordings.ts
@@ -0,0 +1,10 @@
+import { text, integer, sqliteTable } from 'drizzle-orm/sqlite-core'
+import { headsets } from './headsets'
+
+export const recordings = sqliteTable('recordings', {
+  headset: integer('headset').references(() => headsets.id),
+  name: text('name', { length: 256 }),
+  timestamp: text('timestamp')
+})
+
+export type Recording = typeof recordings.$inferSelect
