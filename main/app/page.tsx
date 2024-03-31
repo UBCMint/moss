d8986b30b3287ca8e0256c66781ee2444b14a2ab
Jayden Piao <71306047+jaydenpiao@users.noreply.github.com>
2024-03-31 12:21:00 -0700

    Jaydenpiao/neu 45 server side events with typescript (#12)
    * initialize client for real-time server events
    
    * function to send 100 integers/second
    
    * added type
    
    * 100 integers/second displayed

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -1,12 +1,14 @@
-'use client'
+"use client";
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import { subscribeToIntegers } from "./server/api/utils/dataStream";
 
-export default function Home (): React.JSX.Element {
+export default function Home(): React.JSX.Element {
   /**
    * @TODO Fix type of headsets state
-  */
-  const [headsets, setHeadsets] = useState([])
+   */
+  const [headsets, setHeadsets] = useState([]);
+  const [integers, setIntegers] = useState<number[]>([]);
 
   useEffect(() => {
     const getHeadsets = async (): Promise<void> => {
@@ -14,22 +16,31 @@ export default function Home (): React.JSX.Element {
         const response = await fetch(
           `${process.env.NEXT_PUBLIC_API_ROUTE}/headsets`,
           {
-            method: 'GET',
+            method: "GET",
             headers: {
-              'Content-Type': 'application/json'
-            }
+              "Content-Type": "application/json",
+            },
           }
-        )
-        const data = await response.json()
-        setHeadsets(data)
+        );
+        const data = await response.json();
+        setHeadsets(data);
       } catch (error: any) {
-        console.log(`error: ${error.message}`)
+        console.log(`error: ${error.message}`);
       }
-    }
+    };
     getHeadsets().catch((error) => {
-      console.log(`error: ${error.message}`)
-    })
-  }, [])
+      console.log(`error: ${error.message}`);
+    });
+
+    // Subscribe to the integer stream
+    // callback function called every time a new integer is received
+    const cleanup = subscribeToIntegers((integer) => {
+      // adds new integer to the last 99 integers
+      setIntegers((prevIntegers) => [...prevIntegers.slice(-99), integer]);
+    });
+
+    return () => cleanup(); // Clean up on component unmount
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -39,16 +50,22 @@ export default function Home (): React.JSX.Element {
             MINT&apos;s Neurotech Open Source Project
           </h1>
           <p className="text-2xl p-2 ">
-            Welcome to the MINT&apos;s Neurotech Open Source Project. The following are your headset options:
+            Welcome to the MINT&apos;s Neurotech Open Source Project. The
+            following are your headset options:
           </p>
           {headsets.map((headset: any) => (
-              <div key={headset.id}>
-                <p>{headset.name}</p>
-              </div>
+            <div key={headset.id}>
+              <p>{headset.name}</p>
+            </div>
           ))}
+          <div>
+            <h2 className="text-2xl p-2">Integer Stream:</h2>
+            {integers.map((integer, index) => (
+              <p key={index}>{integer}</p>
+            ))}
+          </div>
         </div>
       </div>
-
     </main>
-  )
+  );
 }
---
+++ main/app/server/api/utils/dataStream.ts
@@ -0,0 +1,33 @@
+import { ServerEventMessage, ServerEventsClient } from "@servicestack/client"
+
+/**
+ * @param {ServerEventMessage} msg
+ * @returns {void}
+ * @description Connects to event stream and logs the integer received from the server
+ */
+const client = new ServerEventsClient("/", ["*"], {
+    handlers: {
+        onInteger: (msg: ServerEventMessage) => {
+            console.log("Received integer:", msg);
+        }
+    },
+    onException: (e:Error) => console.error("Error:", e),
+    onReconnect: () => console.log("Reconnected"),
+}).start()
+
+export default client
+
+/**
+ * @param {number} integer
+ * @returns {void}
+ * @description Sends integers to client 
+ */
+export const subscribeToIntegers = (callback: (integer: number) => void) => {
+    let counter = 0
+    const intervalId = setInterval(() => {
+        callback(counter++)
+        if (counter > 99) counter = 0
+    }, 10) // send integer every 10ms => 100 integers per second
+    // clean up
+    return () => clearInterval(intervalId)
+}
