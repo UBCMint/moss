075e82946432c58cd3e337950bf8cd022d1b3b6f
mona187 <monaabusharkh18@gmail.com>
2024-05-18 13:50:14 -0700

    Revert " database"
    This reverts commit eb55d38fd21d8959d173c4fa9527cf0cef4061bc.

--- main/app/server/db/schema/index.ts
+++ main/app/server/db/schema/index.ts
@@ -1,8 +1,3 @@
 export * from './users'
 export * from './headsets'
 export * from './recordings'
-import { drizzle, type BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
-import Database from 'better-sqlite3'
-
-export const sqlite = new Database('main.db')
-export const db: BetterSQLite3Database = drizzle(sqlite)
