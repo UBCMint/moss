3a2bcefff0a8f6107397859c2cd23d080b19f85a
rachael-peng <127054400+rachael-peng@users.noreply.github.com>
2024-05-18 22:07:13 -0600

    Individual page for each headset

--- main/app/headset/[headsetName]/page.tsx
+++ main/app/headset/[headsetName]/page.tsx
@@ -1,13 +1,65 @@
 import { db } from '@/app/server/db/index'
-// import { GET } from '@/app/server/api/headsets/id/route.ts'
+import { type Headset, headsets } from '@/app/server/db/schema/headsets'
+import { eq, SQLWrapper } from 'drizzle-orm'
 
-
-export default function HeadsetDetails( { params }: {
-    params: {headsetName: string}
-} ) {
-    return (
-        <h1>Details about {params.headsetName} headset</h1>
-    );
+/**
+ * @param params.headsetName
+ * @returns {Promise<Headset[]>}
+ * @description Gets desired headset info from the database
+ */
+async function getHeadsetInfo( params: { headsetName: string } ): Promise<Headset[]> {
+    const headset: Headset[] = await db
+        .select()
+        .from(headsets)
+        .where(eq(headsets.name, params.headsetName))
+        .execute()
+    return headset;
 }
 
-// Use GET Headset by ID route, then display returned data
+/**
+ * @description display the headset info  
+ * @returns null
+ */
+export default async function HeadsetDetails( { params }: {
+    params: {headsetName: string}
+} ) {
+    const headsetInfo = await getHeadsetInfo(params);
+
+    return (
+        <>
+            <h1>Details about {params.headsetName} headset:</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Price ($)</th>
+                        <th>Channel Number</th>
+                        <th>Channel List</th>
+                        <th>Purpose</th>
+                        <th>Portability</th>
+                        <th>Company</th>
+                        <th>Battery Life</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {headsetInfo.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.description}</td>
+                            <td>{item.price}</td>
+                            <td>{item.channelNumber}</td>
+                            <td>{item.channelList}</td>
+                            <td>{item.purpose}</td>
+                            <td>{item.portability}</td>
+                            <td>{item.company}</td>
+                            <td>{item.batteryLife}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+}
