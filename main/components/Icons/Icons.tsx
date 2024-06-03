ec32f81c680611f0de8fd97326d0b463acf872bd
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2024-06-03 11:23:02 -0700

    Multi-select drawer component (#23)

--- main/components/Icons/Icons.tsx
+++ main/components/Icons/Icons.tsx
@@ -106,11 +106,31 @@ function KeyboardArrowDownIcon ({ className, ...props }: SVGIconProps): JSX.Elem
     </svg>
   )
 }
+function CheckIcon ({ className, ...props }: SVGIconProps): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn('size-4', className)}
+      {...props}
+    >
+      <path d="M20 6 9 17l-5-5" />
+    </svg>
+  )
+}
 
 export {
   MintLogo,
   ChevronRightIcon,
   FolderIcon,
   SearchIcon,
-  KeyboardArrowDownIcon
+  KeyboardArrowDownIcon,
+  CheckIcon
 }
