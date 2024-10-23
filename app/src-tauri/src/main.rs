1b5d383041c2f21a1540ae148064555ef62b1e3d
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2024-10-23 16:07:13 -0700

    working tauri build version

---
+++ app/src-tauri/src/main.rs
@@ -0,0 +1,29 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[tauri::command]
+fn on_button_clicked() -> String {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_millis();
+    format!("on_button_clicked called from Rust! (timestamp: {since_the_epoch}ms)")
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![on_button_clicked])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
